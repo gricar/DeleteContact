0c3a63ae5c0ec931f9533bed5b4f3f1c3dbe212f
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2025-03-22 11:32:23 -0300

    Adicionar arquivos de projeto.

---
+++ src/DeleteContact.API/Controllers/ContactsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static Microsoft.AspNetCore.Http.StatusCodes;
+
+namespace DeleteContact.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IMediator _dispatcher;
+
+        public ContactsController(IMediator dispatcher)
+        {
+            _dispatcher = dispatcher ?? throw new ArgumentNullException(nameof(dispatcher));
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(Status204NoContent)]
+        [ProducesResponseType(Status400BadRequest)]
+        [ProducesResponseType(Status404NotFound)]
+        public async Task<ActionResult<CreateContactCommandResponse>> DeleteContact(
+            [FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            await _dispatcher.Send(command, cancellationToken);
+            return NoContent();
+        }
+    }
+}
